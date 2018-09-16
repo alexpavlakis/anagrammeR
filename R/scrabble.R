4feaa5b24d522457f393058b232c9cbe20c4605e
alexpavlakis <alexpavlakis@gmail.com>
2018-09-16 09:22:01 -0400

    small fixes

---
+++ R/scrabble.R
@@ -0,0 +1,7 @@
+#' All words that appear in the scrabble dictionary
+#'
+#' A dataset containing all words in the scrabble dictionary in lowercase.
+#' This is the recommended dataset for looking up words in `get_anagrams`
+#'
+#' @format A character vector with 178692 elements.
+"scrabble"
---
+++ R/words.R
@@ -0,0 +1,7 @@
+#' Standard words file on Unix operating systems.
+#'
+#' A dataset containing all words in the starndard words unix file dictionary in lowercase.
+#' This dataset is somewhat less strict than `scrabble`. (e.g., including 'mr' and 'mrs').
+#'
+#' @format A character vector with 261448 elements.
+"words"
